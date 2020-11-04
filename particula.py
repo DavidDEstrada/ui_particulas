d59fbfc5ee722cb27924069e7495b1db1f04e59d
“David <david.estrada2472@alumnos.udg.mx>
2020-11-04 00:26:12 -0600

    Se crea la funcion para diccionarios

--- particula.py
+++ particula.py
@@ -27,3 +27,16 @@ class Particula:
           'Blue: ' + str(self.__blue)+'\n'+
           'Distancia: ' + str(self.distancia)+ '\n'
         )
+    
+    def to_dict(self):
+        return {
+          "iid": self.__iid,
+          "origen_x": self.__origen_x,
+          "origen_y": self.__origen_y,
+          "destino_x": self.__destino_x,
+          "destino_y": self.__destino_y,
+          "velocidad": self.__velocidad,
+          "red": self.__red,
+          "green": self.__green,
+          "blue": self.__blue
+        }
