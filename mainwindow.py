db143f5d66080fe21ff77864bad979713241d8a1
“David <david.estrada2472@alumnos.udg.mx>
2020-11-14 21:11:20 -0600

    Se crea la funcion para dibujar particulas

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,6 @@
-from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTableWidgetItem
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTableWidgetItem, QGraphicsScene
 from PySide2.QtCore import Slot
+from PySide2.QtGui import QPen, QColor, QTransform
 from ui_mainwindow import Ui_MainWindow
 from particula import Particula
 from swarm import Swarm
@@ -19,6 +20,39 @@ class MainWindow (QMainWindow):
 
         self.ui.mostrar_tabla_pushButton.clicked.connect(self.mostrar_tabla)
         self.ui.buscar_pushButton.clicked.connect(self.buscar_iid)
+
+        self.ui.dibujar_pushButton.clicked.connect(self.dibujar)
+        self.ui.limpiar_pushButton.clicked.connect(self.limpiar)
+        self.scene = QGraphicsScene()
+        self.ui.graphicsView.setScene(self.scene)
+
+    def wheelEvent(self, event):
+        if event.delta() > 0:
+            self.ui.graphicsView.scale(1.2,1.2)
+        else:
+            self.ui.graphicsView.scale(0.8,0.8)
+    @Slot()
+    def dibujar(self):
+        for particula in self.swarm:
+
+            pen = QPen()
+            pen.setWidth(2)
+
+            r = particula.red
+            g = particula.green
+            b = particula.blue
+            color = QColor(r, g ,b)
+            pen.setColor(color)
+
+            self.scene.addEllipse(particula.origen_x, particula.origen_y,3,3, pen)
+            self.scene.addEllipse(particula.destino_x, particula.destino_y,3,3, pen)
+            self.scene.addLine(particula.origen_x + 3, particula.origen_y + 3, particula.destino_x, particula.destino_y, pen)
+
+    @Slot()
+    def limpiar(self):
+        self.scene.clear()
+
+
 
 
     @Slot()
--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -191,6 +191,26 @@ class Ui_MainWindow(object):
         self.gridLayout_5.addWidget(self.mostrar_tabla_pushButton, 1, 2, 1, 1)
 
         self.tabWidget.addTab(self.tab_2, "")
+        self.tab_3 = QWidget()
+        self.tab_3.setObjectName(u"tab_3")
+        self.gridLayout_6 = QGridLayout(self.tab_3)
+        self.gridLayout_6.setObjectName(u"gridLayout_6")
+        self.graphicsView = QGraphicsView(self.tab_3)
+        self.graphicsView.setObjectName(u"graphicsView")
+
+        self.gridLayout_6.addWidget(self.graphicsView, 0, 0, 1, 2)
+
+        self.dibujar_pushButton = QPushButton(self.tab_3)
+        self.dibujar_pushButton.setObjectName(u"dibujar_pushButton")
+
+        self.gridLayout_6.addWidget(self.dibujar_pushButton, 1, 0, 1, 1)
+
+        self.limpiar_pushButton = QPushButton(self.tab_3)
+        self.limpiar_pushButton.setObjectName(u"limpiar_pushButton")
+
+        self.gridLayout_6.addWidget(self.limpiar_pushButton, 1, 1, 1, 1)
+
+        self.tabWidget.addTab(self.tab_3, "")
 
         self.gridLayout_2.addWidget(self.tabWidget, 0, 0, 1, 1)
 
@@ -211,7 +231,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(2)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -246,6 +266,9 @@ class Ui_MainWindow(object):
         self.buscar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Buscar", None))
         self.mostrar_tabla_pushButton.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tabla", None))
+        self.dibujar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Dibujar", None))
+        self.limpiar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Limpiar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("MainWindow", u"Dibujar", None))
         self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
